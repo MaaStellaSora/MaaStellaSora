fa153ceca65fa7ed1f28e90485adc00495cecc60
samipale <seisei_126@163.com>
2026-06-05 20:02:59 +0800

    fix: 修bug

--- agent/custom/reco/climb_tower_potential_bag.py
+++ agent/custom/reco/climb_tower_potential_bag.py
@@ -8,30 +8,49 @@ from custom.reco import climb_tower_potential
 from utils import logger as logger_module
 logger = logger_module.get_logger("climb_tower_potential_bag")
 
-POTENTIAL_XS = [355, 475, 592, 710, 828]
+# 潜能卡片具体定位参数
+POTENTIAL_XS = [358, 475, 593, 710, 828]
 POTENTIAL_W = 118
-POTENTIAL_H = 150
+POTENTIAL_H = 148
+CORE_POTENTIAL_W = 45
+CORE_POTENTIAL_H = 40
+POTENTIAL_LEVEL_W = 50
 POTENTIAL_LEVEL_H = 40
+RECOMMENDED_LEVEL_W = 28
+RECOMMENDED_LEVEL_H = 35
+
+# 潜能画面定位参数
+UPPER_POTENTIAL_FIRST_ROW_Y_OFFSET = 47
+UPPER_POTENTIAL_SECOND_ROW_Y_OFFSET = 211
+LOWER_POTENTIAL_FIRST_ROW_Y_OFFSET = 18
+LOWER_POTENTIAL_SECOND_ROW_Y_OFFSET = 167
+# 划屏结束点
+SCROLL_END = [640, 80]
+
+# 旅人名称位置参数（暂时没用）
 TREKKER_X = 310
 TREKKER_Y_OFFSET = 5
 TREKKER_W = 200
 TREKKER_H = 23 + TREKKER_Y_OFFSET * 2
-UPPER_POTENTIAL_FIRST_ROW_Y_OFFSET = 50
-UPPER_POTENTIAL_SECOND_ROW_Y_OFFSET = 215
-LOWER_POTENTIAL_FIRST_ROW_Y_OFFSET = 18
-LOWER_POTENTIAL_SECOND_ROW_Y_OFFSET = 167
-SCROLL_END = [640, 80]
 
 
 class ScreenBagDataProcessor(climb_tower_potential.ScreenDataProcessor):
     def __init__(self, context: Context):
         super().__init__(context)
+        self.image_reverse = None
+
+    def screenshot(self):
+        super().screenshot()
+        self.image_reverse = ~self.image
 
     def stop_task(self):
         self.context.tasker.post_stop().wait()
 
     def open_bag(self):
-        self.context.run_task("星塔_进入背包界面_agent")
+        self.context.run_task("星塔_打开背包界面_agent")
+
+    def close_bag(self):
+        self.context.run_task("星塔_关闭背包界面_agent")
 
     def click(self, x: int, y: int):
         self.context.tasker.controller.post_click(x, y).wait()
@@ -85,7 +104,7 @@ class ScreenBagDataProcessor(climb_tower_potential.ScreenDataProcessor):
     def get_potential_name_from_bag(self, roi: list[int]) -> str:
         ocr_results = self._ocr("星塔_背包_识别潜能名称_agent", "",roi=roi)
         if not ocr_results:
-            logger.warning("未识别到潜能名称，后续抓取潜能时可能会出现预想外情况")
+            logger.debug("未识别到潜能名称")
             return ""
         return ocr_results[0][0]
 
@@ -97,14 +116,14 @@ class ScreenBagDataProcessor(climb_tower_potential.ScreenDataProcessor):
         return int(ocr_results[0][0])
 
     def get_potential_recommend_level_from_bag(self, roi: list[int]) -> int:
-        ocr_results = self._ocr("星塔_背包_识别推荐等级_agent", "",roi=roi)
+        ocr_results = self._ocr("星塔_背包_识别推荐等级_agent", "",roi=roi, image=self.image_reverse)
         if not ocr_results:
             logger.warning("未识别到推荐等级，将默认为0")
             return 0
         return int(ocr_results[0][0])
 
     def check_potential_recommended_from_bag(self, roi: list[int]) -> bool:
-        return self._color("星塔_背包_识别推荐等级_agent",roi=roi)
+        return self._color("星塔_背包_识别核心潜能_agent",roi=roi)
 
 
 
@@ -145,10 +164,10 @@ class PotentialReader:
                 return []
 
             # 生成潜能列表的roi
-            potential_rois, level_rois, recommend_rois = self._generate_rois(first_y, second_y)
+            potential_rois, core_potential_rois, level_rois, recommend_rois = self._generate_rois(first_y, second_y)
 
             # 然后读取前两行的潜能
-            potentials_upper = self._read_rows(potential_rois, level_rois, recommend_rois)
+            potentials_upper = self._read_rows(potential_rois, core_potential_rois, level_rois, recommend_rois)
 
             # 如果是第一次读取潜能，读取完把光标点回去
             if i == 0:
@@ -168,22 +187,30 @@ class PotentialReader:
             if not first_y or not second_y:
                 logger.error("无法定位潜能位置，扫描背包潜能失败。")
                 return []
-            potential_rois, level_rois, recommend_rois = self._generate_rois(first_y, second_y)
-            potentials_lower = self._read_rows(potential_rois, level_rois, recommend_rois)
+            potential_rois, core_potential_rois, level_rois, recommend_rois = self._generate_rois(first_y, second_y)
+            potentials_lower = self._read_rows(potential_rois, core_potential_rois, level_rois, recommend_rois)
 
             # 识别完一个旅人之后，标记旅人名称
-            potentials = potentials_upper + potentials_lower
-            for potential in potentials:
+            potentials_tmp = potentials_upper + potentials_lower
+            for potential in potentials_tmp:
                 if not potential.trekker:
                     potential.trekker = str(i)
 
+            # 合并临时潜能列表到主潜能列表
+            potentials.extend(potentials_tmp)
+
         # 然后反复三次
         # 读取完潜能后，把数据返回，储存到State中
+        self.screen.close_bag()
         logger.info(f"读取到 {len(potentials)} 个潜能")
         return potentials
 
     def _read_rows(
-            self, potential_rois: list[list[int]], level_rois: list[list[int]], recommend_rois: list[list[int]]
+            self,
+            potential_rois: list[list[int]],
+            core_potential_rois: list[list[int]],
+            level_rois: list[list[int]],
+            recommend_rois: list[list[int]]
     ) -> list[climb_tower_potential.OwnedPotential]:
         """读取潜能"""
         potentials = []
@@ -193,12 +220,13 @@ class PotentialReader:
             potential.name = self.screen.get_potential_name_from_bag(roi)
             if not potential.name:
                 continue
-            # 读取潜能等级
-            potential.level = self.screen.get_potential_level_from_bag(level_rois[i])
-            # 读取推荐等级
-            potential.recommended_level = self.screen.get_potential_recommend_level_from_bag(recommend_rois[i])
             # 读取核心潜能标记
-            potential.core = self.screen.check_potential_recommended_from_bag(roi)
+            potential.core = self.screen.check_potential_recommended_from_bag(core_potential_rois[i])
+            if not potential.core:
+                # 读取潜能等级
+                potential.level = self.screen.get_potential_level_from_bag(level_rois[i])
+                # 读取推荐等级
+                potential.recommended_level = self.screen.get_potential_recommend_level_from_bag(recommend_rois[i])
             # 读取旅人名称（由于意义不大所以不读）
             potential.trekker = ""
             # 保存潜能
@@ -206,15 +234,20 @@ class PotentialReader:
         return potentials
 
     @staticmethod
-    def _generate_rois(y1: int, y2: int) -> tuple[list[list[int]], list[list[int]], list[list[int]]]:
+    def _generate_rois(
+            y1: int, y2: int
+    ) -> tuple[list[list[int]], list[list[int]], list[list[int]], list[list[int]]]:
         """生成潜能列表的roi"""
         potential_rois = [[POTENTIAL_XS[col], y, POTENTIAL_W, POTENTIAL_H]
                           for y in (y1, y2) for col in range(5)]
-        level_rois = [[POTENTIAL_XS[col], y, POTENTIAL_W // 2, POTENTIAL_LEVEL_H]
+        core_potential_rois = [[POTENTIAL_XS[col], y, CORE_POTENTIAL_W, CORE_POTENTIAL_H]
                           for y in (y1, y2) for col in range(5)]
-        recommend_rois = [[POTENTIAL_XS[col] + POTENTIAL_W // 2, y, POTENTIAL_W // 2, POTENTIAL_LEVEL_H]
+        level_rois = [[POTENTIAL_XS[col], y, POTENTIAL_LEVEL_W, POTENTIAL_LEVEL_H]
                           for y in (y1, y2) for col in range(5)]
-        return potential_rois, level_rois, recommend_rois
+        recommend_rois = [
+            [POTENTIAL_XS[col] + POTENTIAL_W - RECOMMENDED_LEVEL_W, y, RECOMMENDED_LEVEL_W, RECOMMENDED_LEVEL_H]
+            for y in (y1, y2) for col in range(5)]
+        return potential_rois, core_potential_rois, level_rois, recommend_rois
 
 
 

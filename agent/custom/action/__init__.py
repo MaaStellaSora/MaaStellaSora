b5635f82dc1c75520543c01696a6b5d81dd73326
samipale <seisei_126@163.com>
2026-06-05 18:20:35 +0800

    refactor: 为方便测试还是把潜能选择模块改为识别了

--- agent/custom/action/__init__.py
+++ agent/custom/action/__init__.py
@@ -1,12 +1,10 @@
 from .climb_tower_shop import *
-from .climb_tower_potential import *
 from .climb_tower_preparation import *
 from .climb_tower_loop import *
 from .invite import *
 from .fight import *
 
 __all__ = [
-    "ChoosePotentialAction",
     "ShopAction",
     "EnhanceAction",
     "InviteAuto",
